3ee1bed6f8d29fa17b2ccd26b41e7ba34ea32efe
Constantin Koehler <Constantin@cadmiumcd.com>
2019-01-20 10:11:20 -0500

    Add Dice 2D Array

---
+++ Boggle/Constants.swift
@@ -0,0 +1,32 @@
+//
+//  Constants.swift
+//  Boggle
+//
+//  Created by Constantin Koehler on 1/20/19.
+//  Copyright © 2019 Constantin Koehler. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    let Dice = [
+        [ "I", "T", "S" , "T", "D", "Y" ],
+        [ "L", "R", "H" , "Z", "N", "N" ],
+        [ "H", "E", "E" , "N", "G", "W" ],
+        [ "I", "E", "R" , "L", "D", "X" ],
+        [ "U", "N", "H" , "I", "Qu", "M" ],
+        [ "T", "W", "A" , "O", "O", "T" ],
+        [ "I", "U", "T" , "C", "O", "M" ],
+        [ "O", "B", "B" , "A", "J", "O" ],
+        [ "A", "F", "P" , "K", "S", "F" ],
+        [ "A", "G", "A" , "E", "N", "E" ],
+        [ "V", "Y", "R" , "D", "L", "E" ],
+        [ "T", "T", "R" , "E", "L", "Y" ],
+        [ "H", "S", "P" , "A", "C", "O" ],
+        [ "R", "E", "T" , "W", "V", "H" ],
+        [ "U", "E", "E" , "S", "I", "N" ],
+        [ "T", "O", "E" , "S", "S", "I" ],
+    ]
+}
+
+

cab11c118ab4de4ee4652f2f7a155fc36c575c6a
Constantin Koehler <Constantin@cadmiumcd.com>
2019-01-20 09:01:47 -0500

    Initial Commit

---
+++ Boggle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Boggle
+//
+//  Created by Constantin Koehler on 1/20/19.
+//  Copyright © 2019 Constantin Koehler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
